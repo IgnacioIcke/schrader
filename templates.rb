435080286a8f2ffe1e7a47c24911b253c11ba236
Ignacio Icke <chabawiki@gmail.com>
2010-03-25 19:11:51 +0100

    i18n and some links added, can be useful atm

--- templates.rb
+++ templates.rb
@@ -1,4 +1,8 @@
 require "erb"
+require 'r18n-desktop'
+
+R18n.from_env 'i18n/'
+include R18n::Helpers
 
 TemplateHeader = %{
     <html><head>
@@ -24,24 +28,24 @@ TemplateDiff = %{
         }
     </script>
     <div class="buttons">
-    <button class="icon icon-next" onClick="location.href = '/'" title="Next"></button>
-    <button class="icon icon-revert" onClick="rollback()" title="rollback"></button>
-    <button class="icon icon-revertwarn" onClick="rollbackwarn()"></button>
-    <button class="icon icon-warn" onClick="location.href = '/'"></button>
-    <button class="icon icon-block" onClick="location.href = '/'"></button>
-    <button class="icon icon-delete" onClick="location.href = '/'"></button>
+    <button class="icon icon-next" onClick="location.href = '/'" title="<%= t.next %>"></button>
+    <button class="icon icon-revert" onClick="rollback()" title="<%= t.rollback %>"></button>
+    <button class="icon icon-revertwarn" onClick="rollbackwarn()" title="<%= t.rollbackandwarn %>"></button>
+    <button class="icon icon-warn" onClick="window.open('<%= @site %>/wiki/Special:Block/<%= @user %>')" title="<%= t.warn %>" ></button>
+    <button class="icon icon-block" onClick="window.open('<%= @site %>/wiki/Special:Block/<%= @user %>')" title="<%= t.block %>"></button>
+    <button class="icon icon-delete" onClick="window.open('<%= @site %>/w/index.php?title=<%= @page %>&action=delete')" title="<%= t.delete %>"></button>
     </div>
     <div class="buttons">
     <button class="icon icon-whitelist" onClick="location.href = '/'"></button>
-    <button class="icon icon-view" onClick="window.open('<%= @site %>/wiki/<%= @page %>')" title="View page"></button>
-    <button class="icon icon-edit" onClick="window.open('<%= @site %>/w/index.php?title=<%= @page %>&action=edit')" title="Edit" ></button>
-    <button class="icon icon-user" onClick="window.open('<%= @site %>/wiki/User:<%= @user %>')" title="User page"></button>
-    <button class="icon icon-talk" onClick="window.open('<%= @site %>/wiki/User Talk:<%= @user %>')" title="User talk"></button>
-    <button class="icon icon-newmsg" onClick="window.open('<%= @site %>/w/index.php?title=User_Talk:<%= @user %>&action=edit&section=new')" title="New message"></button>
+    <button class="icon icon-view" onClick="window.open('<%= @site %>/wiki/<%= @page %>')" title="<%= t.viewpage %>"></button>
+    <button class="icon icon-edit" onClick="window.open('<%= @site %>/w/index.php?title=<%= @page %>&action=edit')" title="<%= t.edit %>" ></button>
+    <button class="icon icon-user" onClick="window.open('<%= @site %>/wiki/User:<%= @user %>')" title="<%= t.userpage %>"></button>
+    <button class="icon icon-talk" onClick="window.open('<%= @site %>/wiki/User Talk:<%= @user %>')" title="<%= t.usertalk %>"></button>
+    <button class="icon icon-newmsg" onClick="window.open('<%= @site %>/w/index.php?title=User_Talk:<%= @user %>&action=edit&section=new')" title="<%= t.newmessage %>"></button>
     </div>
     <center>
     <h1><%= @page %></h1>
-    <span class="centered"><a href="<%= @site %>/wiki/User:<%= @user %>"><%= @user %></a> (<a href="<%= @site %>/wiki/User_Talk:<%= @user %>">talk</a>|<a href="<%= @site %>/wiki/Special:Contributions/<%= @user %>">contributions</a>) </div>
+    <span class="centered"><a href="<%= @site %>/wiki/User:<%= @user %>"><%= @user %></a> (<a href="<%= @site %>/wiki/User_Talk:<%= @user %>"><%= t.talk %></a>|<a href="<%= @site %>/wiki/Special:Contributions/<%= @user %>"><%= t.contributions %></a>) </div>
     <table class="diff">
     <col class="diff-marker"/>
     <col class="diff-content"/>
